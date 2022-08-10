62f0b79beb9c64f3156a43b5c1d8253da5a338de
Sergei Nikiforov <sergei@cur8.co>
2022-08-10 13:04:32 +0000

    save checkpoints less frequently => save some space

--- train_cyclegan.sh
+++ train_cyclegan.sh
@@ -4,4 +4,4 @@ if [ -z "$1" ]
     echo "No number of channels supplied"
     exit 1
 fi
-python3 train.py --dataroot ./datasets/depth --name depth_cyclegan_$1 --model cycle_gan --input_nc $1 --output_nc $1 --display_id 0 --no_html
+python3 train.py --dataroot ./datasets/depth --name depth_cyclegan_$1 --model cycle_gan --input_nc $1 --output_nc $1 --display_id 0 --no_html --save_epoch_freq 10
--- train_pix2pix.sh
+++ train_pix2pix.sh
@@ -4,4 +4,4 @@ if [ -z "$1" ]
     echo "No number of channels supplied"
     exit 1
 fi
-python3 train.py --dataroot ./datasets/depth --name depth_pix2pix_$1 --model pix2pix --input_nc $1 --output_nc $1 --display_id 0 --no_html --dataset_mode aligned
+python3 train.py --dataroot ./datasets/depth --name depth_pix2pix_$1 --model pix2pix --input_nc $1 --output_nc $1 --display_id 0 --no_html --dataset_mode aligned --save_epoch_freq 10
